b246f5f36485fbd855a3e3019b280e210a819b4c
Sylvia Mumbi <mumbi099@gmail.com>
2023-07-23 20:57:32 +0300

    Defined columns and data types for plant model

--- db/migrate/20230723175254_create_plants.rb
+++ db/migrate/20230723175254_create_plants.rb
@@ -1,4 +1,11 @@
 class CreatePlants < ActiveRecord::Migration[6.1]
   def change
+    create_table :plants do |t|
+      t.string :name, null: false
+      t.string :species, null: false
+      t.string :image_url, null: false
+
+      t.timestamps
+    end
   end
 end
