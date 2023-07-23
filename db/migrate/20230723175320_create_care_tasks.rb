29e7fd9ee0f74adfe4559d2550b39058ef2e3644
Sylvia Mumbi <mumbi099@gmail.com>
2023-07-23 20:58:31 +0300

    Defined columns and data types for care-tasks model

--- db/migrate/20230723175320_create_care_tasks.rb
+++ db/migrate/20230723175320_create_care_tasks.rb
@@ -1,4 +1,12 @@
 class CreateCareTasks < ActiveRecord::Migration[6.1]
   def change
+    create_table :care_tasks do |t|
+      t.string :name, null: false
+      t.text :description
+      t.date :due_date, null: false
+      t.references :plant, foreign_key: true
+
+      t.timestamps
+    end
   end
 end
