e04791106e9c1d4d42c3a7d6fca246ecf6395354
Sylvia Mumbi <mumbi099@gmail.com>
2023-07-24 03:13:36 +0300

    Added PATCH and DELETE routes for plant resource

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,9 +21,24 @@ class ApplicationController < Sinatra::Base
     if plant.save
       plant.to_json
     else
-      status 422
       { message: 'Unable to create the plant.' }.to_json
     end
   end
-  
+
+  patch '/plants/:id' do
+    plant = Plant.find(params[:id])
+    if plant.update(plant_params)
+      plant.to_json
+    else
+      { message: 'Unable to update the plant.' }.to_json
+    end
+  end
+
+  delete '/plants/:id' do
+    plant = Plant.find(params[:id])
+    plant.destroy
+    status 200
+    { message: 'Plant has been deleted successfully.' }.to_json
+  end
+
 end
