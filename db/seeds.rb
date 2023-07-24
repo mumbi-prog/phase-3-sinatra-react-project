ccba82ea2c9bf859a316b18ac33c0001fbda5ae4
Sylvia Mumbi <mumbi099@gmail.com>
2023-07-24 16:48:01 +0300

    Created two plants

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,7 @@
 puts "🌱 Seeding spices..."
 
 # Seed your database here
+rose = Plant.create(name: "Rose", species: "Rosa", image_url: "http://example.com/rose.jpg")
+lily = Plant.create(name: "Lily", species: "Lilium", image_url: "http://example.com/lily.jpg")
 
 puts "✅ Done seeding!"
